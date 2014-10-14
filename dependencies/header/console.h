d245b4c346d95f319c9e51bb3afb562fb7588c0d
Edwino Stein <edwino@m2smart.com.br>
2014-10-13 21:11:19 -0400

    Adicionando modulo do console

---
+++ dependencies/header/console.h
@@ -0,0 +1,115 @@
+/*
+  Modulo responsavel por simular um console Unix
+  
+  Edwino Stein - 1201324411
+*/
+  
+#if !defined stdlib
+  #include <stdlib.h>
+#endif
+
+#if !defined stdio
+  #include <stdio.h>
+#endif
+
+#if !defined string
+  #include <string.h>
+#endif
+
+#if !defined ctype
+  #include <ctype.h>
+#endif
+
+/* Define os valores booleanos */
+#if !defined stdbool
+  #include <stdbool.h>
+#endif
+
+#include "tads/commands.h"
+#include "keyboard.h"
+
+#define CONSOLE_INPUT_SYMBLE " #> "
+#define CONSOLE_OUTPUT_SYMBLE "   "
+#define CONSOLE_BRACKSPACE "\b \b"
+#define NULL_CHAR '\0'
+
+/*
+	Definição da estrutura da saida de cada inseção de comando no console
+*/
+typedef struct input_cmd{
+	//Referencia ao nó do comando que está presente na lista de comandos
+	cmdNode * statement;
+
+	//Valor do parametro passado junto ao comando
+	char * paramValue;
+} inputCmd;
+
+/*
+	Cria uma lista de comandos que será utilizada na entrada de dados.
+	
+	Parametros:
+		- cmdsPrototype list[]: Array contendo os prototipos dos comandos;
+		- int listSize: Tamanho do array de prototipos;
+
+	Retorno: cmdList *: Lista encadeada contendo os comandos prontos para serem utilizados;
+*/
+cmdList * initCommands(cmdsPrototype list[], int listSize);
+
+/*
+	Verifica se um caractere ASCII é imprimivel.
+
+	Parametros:
+		- int c: Código ASCII do caractere;
+
+	Retorno: bool: True para quando for imprimivel, false para não imprimivel;
+*/
+bool charIsPrintable(int c);
+
+/*
+	Indentifica o comando e seu parametro apartir de uma string.
+	
+	Parametros:
+		- const char * input: String de entrada;
+		- cmdList * cmds: Lista contendo os comandos conhecidos;
+
+	Retorno: inputCmd *: Referencia de um inputCmd, contendo o comando identificado e
+						 seu parametro, caso haja.
+
+ 	OBS: Um ponteiro NULL será retornado caso o comando não seja indentificado dentro da
+ 		 lista de comandos.
+
+*/
+inputCmd * identifyInput(const char * input, cmdList * cmds);
+
+/*
+	Busca comandos que comecem com a string de entrada.
+	
+	Parametros:
+		- const char * input: String de entrada;
+		- cmdList * cmds: Lista contendo os comandos conhecidos;
+
+	Retorno: cmdList *: Lista que contem os comandos que começam com a string de entrada.
+*/
+cmdList * getCmdHint(const char * input, cmdList * cmds);
+
+/*
+	Imprime na tela uma Lista de comandos.
+	
+	Parametros:
+		- cmdList * cmds: Lista contendo que será impressa;
+*/
+void printHintList(cmdList * cmds);
+
+/*
+	Ler um comando do usuário e retorna uma referencia de comando e seu parametro, caso seja conhecido.
+	
+	Parametros:
+		- cmdList * cmds: Lista de comando conhecidos;
+		- int bufferSize: Tamanho do buffer do teclado;
+
+	Retorno: inputCmd *: Referencia do comando informado e seu parametro, caso haja.
+
+	OBS: Um ponteiro NULL será retornado caso o comando não seja indentificado dentro da
+ 		 lista de comandos.
+*/
+inputCmd * getConsoleInput(cmdList * cmds, int bufferSize);
---
+++ dependencies/src/console.c
@@ -0,0 +1,206 @@
+/*
+  Implementação do Modulo console 
+
+  Edwino Stein - 1201324411
+*/
+
+#include "../header/console.h"
+
+cmdList * initCommands(cmdsPrototype list[], int listSize){
+	cmdList *cmdList = newCmdList();
+
+	int i;
+	for(i = 0; i < listSize; i++){
+		addCmd(cmdList, newCmdNode(&list[i]));
+	}
+
+	return cmdList;
+}
+
+bool charIsPrintable(int c){
+	return (c >= 20 && c <= 126);
+}
+
+inputCmd * identifyInput(const char * input, cmdList * cmds){
+	int inputLen = strlen(input), i = 0;
+
+	char * statement = (char *) malloc(inputLen);
+
+	if(statement == NULL){
+		perror("malloc");
+		exit(1);
+	}
+
+	while(input[i] != ' ' && input[i] != NULL_CHAR){
+		statement[i] = input[i];
+		i++;
+	}
+	statement[i++] = '\0';
+
+	cmdNode * c = getFirstCmdNode(cmds);
+
+	while(c != NULL){
+		if(strcmp(statement, c->command->statement) == 0){
+			break;
+		}
+		c = getNextCmdNode(c);
+	}
+	
+	if(c == NULL){
+		return NULL;
+	}
+
+	char * param = NULL;
+	if(c->command->paramType != 'n'){
+
+		param = (char *) malloc(inputLen - i + 1);
+		if(param == NULL){
+			perror("malloc");
+    		exit(1);
+		}
+
+		int j = 0;
+
+		while(input[i] != NULL_CHAR && input[i] !=  ' '){
+			param[j++] = input[i++];
+		}
+		param[j] = '\0';
+
+		if(strlen(param) <= 0){
+			free(param);
+			param = NULL;
+		}
+	}
+
+	inputCmd * iCmd = (inputCmd *) malloc(sizeof(inputCmd));
+
+	if(iCmd == NULL){
+		perror("malloc");
+    	exit(1);
+	}
+
+	iCmd->statement = c;
+	iCmd->paramValue = param;
+
+	return iCmd;
+	
+}
+
+cmdList * getCmdHint(const char * input, cmdList * cmds){
+
+	cmdNode * c = getFirstCmdNode(cmds);
+
+	cmdList * hintCmds = newCmdList();
+
+	char *p = NULL;
+
+	while(c != NULL){
+
+		if(c->command->cmdId >= 0){
+			p = strstr(c->command->statement, input);
+
+			if(p == &c->command->statement[0]){
+				addCmd(hintCmds, newCmdNode(c->command));
+			}
+		}
+
+		c = getNextCmdNode(c);
+	}
+
+	return hintCmds;
+}
+
+void printHintList(cmdList * cmds){
+	cmdNode * c = getFirstCmdNode(cmds);
+	int i = 0;
+	printf("\n%sDica: ", CONSOLE_OUTPUT_SYMBLE);
+	while(c != NULL){
+		printf("%s, ", c->command->statement);
+		i++;
+
+		if(i >= 6){
+			i = 0;
+			printf("\n%s      ", CONSOLE_OUTPUT_SYMBLE);
+		}
+		c = getNextCmdNode(c);
+	}
+	puts("\b\b.\n");
+}
+
+inputCmd * getConsoleInput(cmdList * cmds, int bufferSize){
+	if(bufferSize <= 0){
+		bufferSize = 50;
+	}
+
+	int bufferPointer = 0;
+	char buffer[bufferSize], key;
+
+	strcpy(buffer, "");
+
+	cmdList * hint = NULL;
+
+	fputs(CONSOLE_INPUT_SYMBLE, stdout);
+
+	while((key = getKey()) != KEY_RETURN){
+
+		switch(key){
+
+			case KEY_BSPACE :
+				if(bufferPointer <= 0){
+					continue;
+				}
+
+				buffer[--bufferPointer] = NULL_CHAR;
+				fputs(CONSOLE_BRACKSPACE, stdout);
+			break;
+
+			case KEY_TAB:
+				hint = getCmdHint(buffer, cmds);
+
+				if(hint->total <= 0){
+					continue;
+				}
+
+				if(hint->total > 1){
+					printHintList(hint);
+
+					fputs(CONSOLE_INPUT_SYMBLE, stdout);
+					fputs(buffer, stdout);
+				}
+				else{
+					strcpy(buffer, getFirstCmdNode(hint)->command->statement);
+					bufferPointer = strlen(buffer);
+					buffer[bufferPointer++] = ' ';
+					buffer[bufferPointer] = '\0';
+
+					
+					if(getFirstCmdNode(hint)->command->paramType == 'i'){
+						printf("\n\n%sDica: O \"%s\" comando exige um número positivo como parametro.\n", CONSOLE_OUTPUT_SYMBLE, getFirstCmdNode(hint)->command->statement);
+					}
+
+					printf("\n%s%s", CONSOLE_INPUT_SYMBLE, buffer);
+				}
+
+			break;
+
+			default:
+				if(bufferPointer >= bufferSize){
+					continue;
+				}
+
+				if(charIsPrintable(key)){
+					key = tolower(key);
+					putchar(key);
+					buffer[bufferPointer++] = key;
+					buffer[bufferPointer] = NULL_CHAR;
+				}
+				
+		}
+	}
+
+	if(bufferPointer <= 0){
+		return identifyInput("null", cmds);
+	}
+
+	return identifyInput(buffer, cmds);
+}
